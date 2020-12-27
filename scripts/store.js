31143190efe7417355828094523956f1fff1ae4d
Claudio Campos <claudio.campos@turner.com>
2020-12-27 00:02:11 -0500

    Set up initial data store structure

--- scripts/store.js
+++ scripts/store.js
@@ -1,3 +1,12 @@
 let STORE = {
+    questions: {
+        question: 'What color tint is predominant in The Matrix',
+        answers: [
+            'Green',
+            'Red',
+            'Yellow',
+            'Blue'
+        ]
+    },
     score: 0
 }
