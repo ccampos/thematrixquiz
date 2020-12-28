5269ed7658f4824586126e44eacccd552a419dc8
Claudio Campos <claudio.campos@turner.com>
2020-12-27 22:28:57 -0500

    fix data store structure

--- scripts/store.js
+++ scripts/store.js
@@ -1,12 +1,16 @@
 let STORE = {
-    questions: {
-        question: 'What color tint is predominant in The Matrix',
-        answers: [
-            'Green',
-            'Red',
-            'Yellow',
-            'Blue'
-        ]
-    },
+    questions: [
+        {
+            question: 'What color tint is predominant within The Matrix environment',
+            answers: [
+                'Green',
+                'Red',
+                'Yellow',
+                'Blue'
+            ],
+            correctAnswer: 'Green'
+        }
+    ],
+    index: 0,
     score: 0
 }
